3395ddc57db809371962f2314bd6c9d27bf02325
Casey Celestin <casey.celestin@meetkai.com>
2022-12-07 10:40:51 -0800

    Fix cargo check errors

--- crates/bevy_webxr/src/interaction/input.rs
+++ crates/bevy_webxr/src/interaction/input.rs
@@ -1,9 +1,12 @@
 use bevy_ecs::world::Mut;
 use bevy_log::info;
 use bevy_xr::XrActionSet;
+use wasm_bindgen::JsValue;
 
 pub fn handle_input(action_set: Mut<XrActionSet>, frame: web_sys::XrFrame) {
     // frame.session().input_sources()
     info!("Input is being handled");
-    web_sys::console::log("Input is being handled");
+    web_sys::console::log(&js_sys::Array::from(&JsValue::from_str(
+        "Input is being handled",
+    )));
 }
