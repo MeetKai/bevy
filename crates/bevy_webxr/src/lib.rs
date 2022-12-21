85039da7c91454b6f79c608c36ee7dd71cb572a4
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2022-12-21 16:22:04 -0300

    Invert the Y axis because our world is Y upside down

--- crates/bevy_webxr/src/lib.rs
+++ crates/bevy_webxr/src/lib.rs
@@ -19,10 +19,9 @@ use bevy_render::{
     camera::{Camera, ManualTextureViews, RenderTarget, Viewport},
     renderer::{RenderAdapterInfo, RenderDevice, RenderQueue},
 };
-use bevy_transform::prelude::{TransformBundle, Transform};
+use bevy_transform::prelude::{Transform, TransformBundle};
 use bevy_utils::{default, Uuid};
 use initialization::InitializedState;
-use interaction::TrackingSource;
 use std::{cell::RefCell, rc::Rc, sync::Arc};
 use wasm_bindgen::{prelude::Closure, JsCast};
 use web_sys::XrWebGlLayer;
@@ -150,7 +149,7 @@ fn setup_webxr_pawn(
                 RightEyeMarker,
             ));
         });
-        
+
     bevy_log::info!("finished setup");
 }
 
@@ -264,15 +263,6 @@ fn webxr_runner(mut app: App) {
 
     *g.borrow_mut() = Some(Closure::new(move |_time: f64, frame: web_sys::XrFrame| {
         app.world.insert_non_send_resource(frame.clone());
-
-        let webxr_context = app.world.get_non_send_resource::<WebXrContext>().unwrap();
-
-        let (space, space_type) = webxr_context.space_info.clone();
-        // update the current frame inside tracking soource
-        let tracking_source = TrackingSource::new(space, space_type, frame.clone());
-        // Resource used to track poses
-        app.world
-            .insert_resource(bevy_xr::XrTrackingSource::new(Box::new(tracking_source)));
 
         app.update();
 
