f0feaab7ea25efab0bd1b389ea64ec523e05f579
Dekuraan <declan.li.carney@meetkai.com>
2023-01-10 11:46:01 -0500

    fix missing import

--- crates/bevy_webxr/src/util.rs
+++ crates/bevy_webxr/src/util.rs
@@ -1,5 +1,5 @@
 //Gets y fov in radians from a perspective matrix
-pub fn fov_from_mat4(mat: Mat4) -> f32 {
+pub fn fov_from_mat4(mat: bevy_math::Mat4) -> f32 {
     let f = mat.y_axis.y;
     let fov_y_radians = 2.0 * (1.0 / f).atan();
     fov_y_radians
